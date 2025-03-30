066713df427b884c2e123e066008a49f553efa15
francescopasta <pastore.francesco01@gmail.com>
2025-03-30 15:44:55 +0200

    Trying to understand what Instersect() and Intersects() functions mean

--- Assets/DungeonGeneratorNew.cs
+++ Assets/DungeonGeneratorNew.cs
@@ -12,9 +12,10 @@ public class DungeonGeneratorNew : MonoBehaviour
     RectInt roomFirst = new RectInt(0,0,100,50);
     List<RectInt> rooms;
     List<RectInt> roomsUsed = new List<RectInt>();
-    List<RectInt> theDoors = new List<RectInt>();
+    //List<RectInt> theDoors = new List<RectInt>();
 
-    public float animationTime;
+    public float animationTimeRooms;
+    public float animationTimeDoors;
     public int limit = 10;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,7 +27,7 @@ public class DungeonGeneratorNew : MonoBehaviour
 
     IEnumerator AnimateCut()
     {
-        yield return new WaitForSeconds(animationTime);
+        yield return new WaitForSeconds(animationTimeRooms);
         //int boolean = ;
         if(Random.Range(2,0) % 2 == 0)
         {
@@ -41,7 +42,7 @@ public class DungeonGeneratorNew : MonoBehaviour
             //if (rooms[i].width < limit * 2 && rooms[i].height < limit * 2) continue;
             if(rooms[i].width >= rooms[i].height && rooms[i].width > limit * 2)
             {
-                yield return new WaitForSeconds(animationTime);
+                yield return new WaitForSeconds(animationTimeRooms);
                 List<RectInt> list = new List<RectInt>(CutterWidth(rooms[i]));
                 foreach (var room in list)
                 {
@@ -52,7 +53,7 @@ public class DungeonGeneratorNew : MonoBehaviour
             else
             {
                 if (rooms[i].height < limit * 2) continue;
-                yield return new WaitForSeconds(animationTime);
+                yield return new WaitForSeconds(animationTimeRooms);
                 List<RectInt> list = new List<RectInt>(CutterHeight(rooms[i]));
                 foreach (var room in list)
                 {
@@ -61,11 +62,41 @@ public class DungeonGeneratorNew : MonoBehaviour
                 roomsUsed.Add(rooms[i]);
             }
         }
+
+        StartCoroutine(AddDoors());
+    }
+
+    IEnumerator AddDoors()
+    {
+        //RectInt intersector = AlgorithmsUtils.Intersect(roomsUsed[0], roomsUsed[1]);
+        //AlgorithmsUtils.DebugRectInt(intersector, Color.red, float.MaxValue);
+
+        foreach (var room in roomsUsed)
+        {
+            for (global::System.Int32 i = 0; i < roomsUsed.Count; i++)
+            {
+                //if(AlgorithmsUtils.Intersects(room, roomsUsed[i]))
+                //{
+                    RectInt intersector = AlgorithmsUtils.Intersect(room, roomsUsed[i]);
+                    AlgorithmsUtils.DebugRectInt(intersector, Color.red, float.MaxValue);
+                    //AlgorithmsUtils.FillRectangleOutline(["A", "B"], intersector, "A");
+                    yield return new WaitForSeconds(animationTimeDoors);
+                //}
+
+                //if (AlgorithmsUtils.Intersects(room, roomsUsed[i]))
+                //{
+                //    RectInt door = new RectInt(X + roomA.width, Random.Range(Y, Y + roomCut.height), 1, 1);
+                //    StartCoroutine(ShowRect(door));
+                //    AlgorithmsUtils.DebugRectInt(door, Color.yellow, float.MaxValue);
+                //}
+            } 
+        }
+
     }
 
     IEnumerator ShowRect(RectInt rect)
     {
-        yield return new WaitForSeconds(animationTime);
+        yield return new WaitForSeconds(animationTimeDoors);
         AlgorithmsUtils.DebugRectInt(rect, Color.red, float.MaxValue);
     }
 
@@ -80,8 +111,8 @@ public class DungeonGeneratorNew : MonoBehaviour
         RectInt roomA = new RectInt(X, Y, roomCut.width - (int)halfWidth, roomCut.height);
         RectInt roomB = new RectInt(X + roomA.width + 1, Y, (int)halfWidth - 1, roomCut.height);
         //Display the two new Rects
-        AlgorithmsUtils.DebugRectInt(roomA, Color.blue, float.MaxValue);
-        AlgorithmsUtils.DebugRectInt(roomB, Color.blue, float.MaxValue);
+        AlgorithmsUtils.DebugRectInt(roomA, Color.black, float.MaxValue);
+        AlgorithmsUtils.DebugRectInt(roomB, Color.black, float.MaxValue);
 
         ////ADD RANDOM DOOR
 
@@ -111,8 +142,8 @@ public class DungeonGeneratorNew : MonoBehaviour
         RectInt roomA = new RectInt(X, Y, roomCut.width,  roomCut.height - (int)halfHeight);
         RectInt roomB = new RectInt(X, Y + roomA.height + 1, roomCut.width, (int)halfHeight - 1);
         //Display the two new Rects
-        AlgorithmsUtils.DebugRectInt(roomA, Color.blue, float.MaxValue);
-        AlgorithmsUtils.DebugRectInt(roomB, Color.blue, float.MaxValue);
+        AlgorithmsUtils.DebugRectInt(roomA, Color.black, float.MaxValue);
+        AlgorithmsUtils.DebugRectInt(roomB, Color.black, float.MaxValue);
 
         ////ADD RANDOM DOOR
 
